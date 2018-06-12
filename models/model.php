c50569b4907d4f46326ae8f45d5cb70eab09fa48
NJacq <nicolas.j@codeur.online>
2018-06-12 17:08:45 +0200

    affichage ok

--- models/model.php
+++ models/model.php
@@ -1,14 +1,68 @@
 
+
+
 <?php
+
+
+// 
+// $db = new PDO('mysql:host=localhost;dbname=move_watch;charset=utf8','admin','Ma Ch1p3tt3!');
+
+// $sql = $db->prepare('SELECT * FROM tournages');
+// $sql->execute();
+// $response = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+// echo json_encode($response, JSON_UNESCAPED_UNICODE);
+
+
 $db = new PDO('mysql:host=localhost;dbname=move_watch;charset=utf8','admin','Ma Ch1p3tt3!');
 
-$sql = $db->prepare('SELECT * FROM test');
-$sql->execute();
-$response = $sql->fetchAll(PDO::FETCH_ASSOC);
+	
+	$req = $db->prepare("SELECT * FROM `tournages` WHERE ardt = '2ème'"); /*requète vers la table*/
+	
+$req->execute();
+	while($geo = $req->fetch())   /*génération du tableau */
+    {
+	
+	$tournage[] = array(  															 /*tableau des objets*/
+						 'type' 		=> 'Feature',  								 /*en tête de l'objet voir http://geojson.org/*/
+						 'geometry'	=> array(
+                                                 'type' => 'Point', 					 /*type de l'objet*/
+                                                 'coordinates'=>array(floatval($geo['y']),floatval($geo['x']))),
+												// 'coordinates' => "[".$geo['y'].",".$geo['x']."]"),
+						 'properties' => array(
+												'titre' => $geo['titre'], /* concaténation des variables dates et nom pour l'affichage du titre*/
+                                                'adresse' => $geo['adresse'],/* concaténation des variables  pour l'affichage des details*/ 
+                                                'realisateur' => $geo['realisateur'],
+                                                'type_de_tournage' => $geo['type_de_tournage'],
+                                                'organisme_demandeur' => $geo['organisme_demandeur'],
+                                                'ardt' => $geo['ardt'],
+                                                'xy'=>array(floatval($geo['x']),floatval($geo['y']))
+												));
+	
+	}
+echo json_encode($tournage,  JSON_UNESCAPED_UNICODE); /*encodage de l'array $formation*/
 
-echo json_encode($response, JSON_UNESCAPED_UNICODE);
+// print_r($geo);
 
 
+// $tournages = file_get_contents('../view/tournages.geojson', 'r+');  /* ouverture du fichier geojson*/
+
+
+// file_put_contents($tournages, $geo, FILE_APPEND | LOCK_EX); /* ecriture des points récupérés depuis la base de données*/
+
+
+// // fclose($tournages); /* fermeture du fichier geojson*/									
+															
+									
+// // header('Location: form_carto.php'); /* renvoie vers la le formulaire*/	
+
+
+// $fp = fopen('../view/tournages.geojson', 'a');//opens file in append mode  
+// fwrite($fp, $geo);  
+// // fwrite($fp, 'appending data');  
+// fclose($fp);  
+																	
+?>
 
 
 
