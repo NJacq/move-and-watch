956a921562a10e5228e701471d3ce6feda9b7351
claireb25 <claire.bourgeois.cb@gmail.com>
2018-06-12 17:43:35 +0200

    test

--- utils/bdd.php
+++ utils/bdd.php
@@ -2,7 +2,7 @@
 
 try{
   
-    $bdd = new PDO('mysql:host=localhost;dbname=move_watch;charset=utf8','admin','Ma Ch1p3tt3!');
+    $bdd = new PDO('mysql:host=localhost;dbname=move_watch;charset=utf8','admin','online2017');
 
 }
 catch(Exeption $e)
