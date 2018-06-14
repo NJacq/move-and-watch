0c6ce868ba8bbfce4367cd0696ee2112917239df
claireb25 <claire.bourgeois.cb@gmail.com>
2018-06-14 14:56:01 +0200

    filtres en ajax, html partie filtres OK

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -4,12 +4,33 @@ var RealVue = new Vue({
     data: {
         items:[]
     },
-    mounted : function () {
+    beforeCreate : function () {
         var self = this;
         $.ajax({
             url: 'models/model_rea.php',
             success: function (response) {
-               self.items = JSON.parse(response);      
+               self.items = JSON.parse(response);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }
+});
+
+var CheckboxVue = new Vue({
+    el: '#types',
+    datatype: JSON,
+    data: {
+        items:[]
+    },
+    beforeCreate : function () {
+        var self = this;
+        $.ajax({
+            url: 'models/model_for.php',
+            success: function (response) {
+               self.items = JSON.parse(response);
+               console.log(self.items);
             },
             error: function (error) {
                 console.log(error);
