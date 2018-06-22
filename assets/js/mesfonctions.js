fc637cbed0addd7e586dff82b9b85420b154c93b
NJacq <nicolas.j@codeur.online>
2018-06-22 10:59:01 +0200

    new carte ok

--- assets/js/mesfonctions.js
+++ assets/js/mesfonctions.js
@@ -1,177 +1,150 @@
-var mymap = L.map('map').setView([48.8566, 2.3522], 12);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoibmljb2xhc2phYyIsImEiOiJjamk1czB5YXowbjhyM2txb2Y4YXVnbjQxIn0.5PuCZmNu4RWZu1FlU0Kbqg', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1Ijoibmljb2xhc2phYyIsImEiOiJjamk1czB5YXowbjhyM2txb2Y4YXVnbjQxIn0.5PuCZmNu4RWZu1FlU0Kbqg'
-}).addTo(mymap);
 
+  var malongitude="";
+  var malatitude="";
+  var map = "";
+  setTimeout("setPosition();",900);
 
-// L.Routing.control({
-//     waypoints: [
-//       L.latLng(57.74, 11.94),
-//       L.latLng(57.6792, 11.949)
-//     ]
-//   }).addTo(mymap);
-
-
-function createButton(label, container) {
-    var btn = L.DomUtil.create('button', '', container);
-    btn.setAttribute('type', 'button');
-    btn.innerHTML = label;
-    return btn;
-}
-
-// mymap.on('click', function (e) {
-//     var container = L.DomUtil.create('div'),
-//         startBtn = createButton('Start from this location', container),
-//         destBtn = createButton('Go to this location', container);
-
-//     L.popup()
-//         .setContent(container)
-//         .setLatLng(e.latlng)
-//         .openOn(mymap);
-// });
-
-mymap.locate({
-    setView: true,
-    maxZoom: 15
-});
-
-function onLocationFound(e) {
-    var radius = e.accuracy / 2;
-    var iconeLoc = L.icon({
-        iconUrl: "assets/media/point_loc.png",
-        iconSize: [8, 8]
-    });
-    
-    $('#data').on('click', '#recenter', function () {
-        mymap.panTo({
-            lon: mymap.initlng,
-            lat: mymap.initlat
-        }, {
-            'animate': true
-        });
-    });
-        
-        L.marker(e.latlng,{icon:iconeLoc}).addTo(mymap)
-        // .bindPopup("Vous êtes ici").openPopup();
-
-        L.circle(e.latlng, radius).addTo(mymap);
-    mymap.initlat = e.latlng.lat;
-    mymap.initlng = e.latlng.lng;
-}
-
-mymap.on('locationfound', onLocationFound);
-
-function onLocationError(e) {
-    alert(e.message);
-}
-
-mymap.on('locationerror', onLocationError);
-mymap.on('move', function () {
-    var zm = mymap.getZoom();
-    var ctr = mymap.getCenter();
-    var ll = 'ZOOM:' + zm + ' | MAPCENTER: Lat: ' + ctr['lat'].toFixed(4) + ' Lng: ' + ctr['lng'].toFixed(4) + ' | <span id="recenter">Re-center</span>';
-    $('#data').html(ll);
-});
-
-
-
-var tournages = $.getJSON("models/model.php", function (dataTournages) {
-    var iconeTournage = L.icon({
-        iconUrl: "assets/media/clap.png",
-        iconSize: [19, 21]
-    });
-    // L.geoJSON(dataTournages,{
-    //     pointToLayer:function(feature,latlng){
-    //         var marker=L.marker(latlng,{icon:iconeTournage});
-    //         marker.bindPopup('<b><u>Description du tournage</u></b><br>'
-    //         +'<b>Nom du film : </b>' +feature.properties.titre+'</br>'
-    //         +'<b>Réalisateur : </b>' +feature.properties.realisateur+'</br>'
-    //         +'<b>Format : </b>' +feature.properties.type_de_tournage+'</br>'
-    //         +'<b>Organisme demandeur : </b>' +feature.properties.organisme_demandeur+'</br>'
-    //         +'<b>Adresse : </b>' +feature.properties.adresse+'</br>'
-    //         +'<b>Arrondissement : </b>' +feature.properties.ardt+'</br>'
-    //     );
-    //         return marker;
-    //     }
-    // }).addTo(mymap);
-
-    L.geoJson(dataTournages, {
-       
-        onEachFeature:function(feature, layer) {
-            
+function setPosition(){
+  if(navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(position);
+}      
   
-            layer.on({
-                click: function showResultsInDiv() {
-                    var d = document.getElementById('click-tournage');
-                    
-                    d.innerHTML = "";
-
-                 
-                    d.innerHTML +=
-                        '<h4>Tout savoir sur:</h4>'+ 
-                          feature.properties.titre + '</br>' +
-                        '<b>Réalisateur : </b>' + feature.properties.realisateur + '</br>' +
-                        '<b>Format : </b>' + feature.properties.type_de_tournage + '</br>' +
-                        '<b>Organisme demandeur : </b>' + feature.properties.organisme_demandeur + '</br>' +
-                        '<b>Adresse : </b>' + feature.properties.adresse + '</br>' +
-                        '<b>Arrondissement : </b>' + feature.properties.ardt + '</br>'
-                        
-                }
+  
 
 
-            });
-        }
-    }).addTo(mymap);
+function showCarte( malatitude, malongitude ){ 
+  L.mapquest.key = 'MAxEGKsMuGoi5Y7FMzaxC1RirEkOI5xg';
+  
+  //console.log( malatitude );
+  var map = L.mapquest.map('map', {     
+    center: [malatitude,malongitude],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 13
+  });    
+  map.addControl(L.mapquest.control());
+  map.addControl(L.mapquest.geocodingControl({
+    position: 'topleft'
+  }));
+
+  L.mapquest.directionsControl({
+    routeSummary: {
+      enabled: true
+      
+    },
+    narrativeControl: {
+      enabled: true,
+      compactResults: false,
+      interactive: true
+
+    }
+  }).addTo(map);
+
+  L.marker([malatitude,malongitude], {
+    icon: L.mapquest.icons.flag({
+      primaryColor: '#22407F',
+      secondaryColor: '#3B5998',
+      shadow: true,
+      size: 'md',
+      symbol: 'Ici'
+    })
+  }).addTo(map);
+
+
+
     
+  $.getJSON('./models/model.php', function(data) {
+    L.geoJSON(data, {
+      
+      onEachFeature:function(feature, layer) {
+        
 
+        layer.on({
+            click: function showResultsInDiv() {
+                var d = document.getElementById('click-tournage');
+              
+                d.innerHTML = "";
 
+            
+                d.innerHTML +=
+                    '<h4>Tout savoir sur:</h4>'+ 
+                      feature.properties.titre + '</br>' +
+                    '<b>Réalisateur : </b>' + feature.properties.realisateur + '</br>' +
+                    '<b>Format : </b>' + feature.properties.type_de_tournage + '</br>' +
+                    '<b>Organisme demandeur : </b>' + feature.properties.organisme_demandeur + '</br>' +
+                    '<b>Adresse : </b>' + feature.properties.adresse + '</br>' +
+                    '<b>Arrondissement : </b>' + feature.properties.ardt + '</br>'
+            }
+          });
+        }
+      }).addTo(map);
+    }); 
+}
 
-});
+    
+         
+function position(pos){    
+      var malatitude= pos.coords.latitude;
+      var malongitude= pos.coords.longitude;
+      showCarte(malatitude, malongitude);
+      
+}
 
-var routing = new L.Routing({
-    position: 'topright'
-    ,routing: {
-      router: myRouterFunction
-    }
-    ,tooltips: {
-      waypoint: 'Waypoint. Drag to move; Click to remove.',
-      segment: 'Drag to create a new waypoint'
-    }
-    ,styles: {     // see http://leafletjs.com/reference.html#polyline-options
-      trailer: {}  // drawing line
-      ,track: {}   // calculated route result
-      ,nodata: {}  // line when no result (error)
-    }
-    ,snapping: {
-      layers: [mySnappingLayer]
-      ,sensitivity: 15
-      ,vertexonly: false
-    }
-    ,shortcut: {
-      draw: {
-        enable: 68    // 'd'
-        ,disable: 81  // 'q'
-      }
-    }
+function geo(){
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position);
+  }
+}
+
+function carte(){
+  
+
+  $.getJSON('./models/model.php', function(data) {
+    L.geoJSON(data, {
+       
+               onEachFeature:function(feature, layer) {
+                  
+        
+                   layer.on({
+                       click: function showResultsInDiv() {
+                           var d = document.getElementById('click-tournage');
+                          
+                           d.innerHTML = "";
+      
+                       
+                           d.innerHTML +=
+                               '<h4>Tout savoir sur:</h4>'+ 
+                                 feature.properties.titre + '</br>' +
+                               '<b>Réalisateur : </b>' + feature.properties.realisateur + '</br>' +
+                               '<b>Format : </b>' + feature.properties.type_de_tournage + '</br>' +
+                               '<b>Organisme demandeur : </b>' + feature.properties.organisme_demandeur + '</br>' +
+                               '<b>Adresse : </b>' + feature.properties.adresse + '</br>' +
+                               '<b>Arrondissement : </b>' + feature.properties.ardt + '</br>'
+                       }
+                  });
+               }
+           }).addTo(map);
   });
-  mymap.addControl(routing);
+
+  L.mapquest.directionsControl({
+    routeSummary: {
+      enabled: true
+     
+    },
+    narrativeControl: {
+      enabled: true,
+      compactResults: false,
+      interactive: true
+
+    }
+  }).addTo(map);
 
 
 
-
-
-
-
-
-// L.DomEvent.on(startBtn, 'click', function () {
-//     control.spliceWaypoints(0, 1, e.latlng);
-//     map1.closePopup();
-// });
-
-// L.DomEvent.on(destBtn, 'click', function () {
-//     control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
-//     map1.closePopup();
-// });
+  document.addEventListener('click', function(event){
+    console.log(event.target);
+    console.log(document.querySelector('.leaflet-popup-content').innerText);
+    })
+  
+};
+      
+    
