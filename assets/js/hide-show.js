b335aba80a75183c3a1047d4f312310da07e5cd3
claireb25 <claire.bourgeois.cb@gmail.com>
2018-06-26 18:45:24 +0200

    modifs du css pour que la div filtres apparaisse au bon endroit avec un background blanc

--- assets/js/hide-show.js
+++ assets/js/hide-show.js
@@ -1,7 +1,6 @@
 
 // apparition et disparaition de la div Filtres 
     $("#exit").click(function(event){ // exit = id de la croix
-    
         event.preventDefault(); 
         $("#filtres").hide(1000);// #filtres = id de la div filtres 
     });
